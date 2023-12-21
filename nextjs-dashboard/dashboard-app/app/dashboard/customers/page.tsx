c5642b6d30b87b0b7183f20ab93c29c85f5955b3
Konstantinos Leimonis <k.leimonis@gmail.com>
2023-12-21 16:28:27 +0000

    feat: setup application with bit

---
+++ nextjs-dashboard/dashboard-app/app/dashboard/customers/page.tsx
@@ -0,0 +1,26 @@
+import { fetchFilteredCustomers } from '../../lib/data';
+import CustomersTable from '../../ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
+}
